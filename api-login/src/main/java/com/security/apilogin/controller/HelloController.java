842d0a762fa54448eca528a72c22eb89f949af9d
sidahmed.faisal <sidahmed.faisal@nctr.sd>
2023-03-15 10:06:04 +0200

    add securityconfig

---
+++ api-login/src/main/java/com/security/apilogin/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.security.apilogin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+public class HelloController {
+    @GetMapping("/")
+    public String greeting(){
+        return "Hello World";
+    }
+}
