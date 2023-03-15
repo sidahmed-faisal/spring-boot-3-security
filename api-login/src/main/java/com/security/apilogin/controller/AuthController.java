f3006adb1aa57bd0ca9d697a5c3ec85968644025
sidahmed.faisal <83870963+sidahmed-faisal@users.noreply.github.com>
2023-03-15 11:16:21 +0200

    Creation of token using issuer form Loginrequest

--- api-login/src/main/java/com/security/apilogin/controller/AuthController.java
+++ api-login/src/main/java/com/security/apilogin/controller/AuthController.java
@@ -2,19 +2,32 @@ package com.security.apilogin.controller;
 
 import com.security.apilogin.model.LoginRequest;
 import com.security.apilogin.model.LoginResponse;
+import com.security.apilogin.security.JwtIssuer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
+
+    //using final is same as creating a constructor like this:
+//    public AuthController(JwtIssuer jwtIssuer) {
+//        this.jwtIssuer = jwtIssuer;
+//    }
+    private final JwtIssuer jwtIssuer;
+
+
+
     @PostMapping("/auth/login")
     public LoginResponse login(@RequestBody @Validated LoginRequest request){
+        var token = jwtIssuer.issue(1L ,request.getEmail() , List.of("USER"));
         return LoginResponse.builder()
-                .accessToken("anything")
+                .accessToken(token)
                 .build();
     }
 }
